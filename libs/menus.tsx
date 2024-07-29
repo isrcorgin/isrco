a516fc9039440d9ecb9abdb065e0aa600697bb77
Your Name <isrc.org.in@gmail.com>
2024-07-29 17:17:27 +0530

    isrco

---
+++ libs/menus.tsx
@@ -0,0 +1,33 @@
+export const menus = [
+  {
+    label: "Home",
+    link: "/",
+  },
+
+  {
+    label: "About us",
+    link: "/about-us/",
+  },
+
+  {
+    label: "Campus Ambassador",
+    link: "/campus-ambassador/",
+   
+  },
+
+  {
+    label: "Event",
+    link: "/event/",
+  },
+
+  {
+    label: "Verify Certificate",
+    link: "/verify",
+  },
+
+  {
+    label: "Login",
+    link: "/auth/login/",
+  },
+
+];
