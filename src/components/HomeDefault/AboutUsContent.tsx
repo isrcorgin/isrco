8509b8157bea3c49a647d9f9fcbecbfb0b7bd42a
Your Name <isrc.org.in@gmail.com>
2024-07-30 19:10:57 +0530

    done2

--- src/components/HomeDefault/AboutUsContent.tsx
+++ src/components/HomeDefault/AboutUsContent.tsx
@@ -8,47 +8,27 @@ const AboutUsContent: React.FC = () => {
       <div className="about-area ptb-120 bg-image">
         <div className="container">
           <div className="row h-100 align-items-center">
-          <div className="col-lg-6">
-  <div className="about-content">
-    <span>Join The Event</span>
-    <h3>
-      Join the <b>Global</b> Innovation Spectacle!
-    </h3>
-    <p>
-      Welcome to the International STEM and Robotics Championship (ISRC)!
-      Join the global stage where over 4,000 teams from 15 countries compete in Innovation and Robotics categories. Whether you're in grades 5-8, grades 9-12, or a college freshman, ISRC offers a unique platform to showcase your skills.
-    </p>
-    <ul>
-      <li><b>STEM Excellence:</b> Aligning with government initiatives to promote STEM education.</li>
-      <li><b>Global Community:</b> Connect with peers, mentors, and industry leaders.</li>
-      <li><b>Three Rounds:</b> Online quizzes, hands-on competitions, and a thrilling finale.</li>
-      <li><b>Prizes:</b> Win prestigious awards and recognition.</li>
-      <li><b>Professional Development:</b> Aiming to train 10 million teachers in STEM by 2030.</li>
-      <li><b>Partnership Opportunities:</b> Partner with us to promote STEM globally.</li>
-    </ul>
-    {/* Uncomment the following section if needed */}
-    {/* 
-    <div className="signature">
-      <Image
-        src="/images/signature.png"
-        alt="signature"
-        width={142}
-        height={68}
-      />
-    </div>
-
-    <Link href="/about-us" className="btn btn-primary">
-      Read More
-      <i className="icofont-double-right"></i>
-    </Link>
-
-    <Link href="#" className="btn btn-secondary">
-      Buy Ticket
-    </Link> 
-    */}
-  </div>
-</div>
-
+            <div className="col-lg-6">
+              <div className="about-content">
+                <span>Join The Event</span>
+                <h3>
+                  Join the <b>Global</b> Innovation Spectacle!
+                </h3>
+                <p>
+                  Welcome to the International STEM and Robotics Championship (ISRC)!
+                  Join the global stage where over 4,000 teams from 15 countries compete in Innovation and Robotics categories. Whether you&apos;re in grades 5-8, grades 9-12, or a college freshman, ISRC offers a unique platform to showcase your skills.
+                </p>
+                <ul>
+                  <li><b>STEM Excellence:</b> Aligning with government initiatives to promote STEM education.</li>
+                  <li><b>Global Community:</b> Connect with peers, mentors, and industry leaders.</li>
+                  <li><b>Three Rounds:</b> Online quizzes, hands-on competitions, and a thrilling finale.</li>
+                  <li><b>Prizes:</b> Win prestigious awards and recognition.</li>
+                  <li><b>Professional Development:</b> Aiming to train 10 million teachers in STEM by 2030.</li>
+                  <li><b>Partnership Opportunities:</b> Partner with us to promote STEM globally.</li>
+                </ul>
+              
+              </div>
+            </div>
 
             <div className="col-lg-6">
               <div className="about-image">
