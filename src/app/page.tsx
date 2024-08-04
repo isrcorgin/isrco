7fdd05530e99eda291e1ba1a603c87b59d86a5ae
Your Name <isrc.org.in@gmail.com>
2024-08-04 12:02:14 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Navbar from "@/components/Layouts/Navbar";
 import MainBanner from "@/components/HomeDefault/MainBanner";
 import AboutUsContent from "@/components/HomeDefault/AboutUsContent";
-import WhyUs from "@/components/Common/WhyUs";
+
 import Speakers1 from "@/components/HomeThree/Speakers";
-import Speakers from "@/components/HomeDefault/Speakers";
-import EventSchedules from "@/components/HomeDefault/EventSchedules";
-import FunFact from "@/components/Common/FunFact";
-import Pricing from "@/components/HomeDefault/Pricing";
+
+
+
+
 import Partner from "@/components/Common/Partner";
-import LatestNews from "@/components/HomeDefault/LatestNews";
+
 import BuyTicket from "@/components/Common/BuyTicket";
 import Subscribe from "@/components/Common/Subscribe";
 import Footer from "@/components/Layouts/Footer";
